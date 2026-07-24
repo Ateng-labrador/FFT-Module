21ad1b68e81f4e7fe6e648a0831d0d51a59f8c61
Ateng-labrador <arlantengga8@gmail.com>
2026-07-24 16:39:32 +0700

    fix: fix-bug

--- example/signal/testing_blue_einstein.py
+++ example/signal/testing_blue_einstein.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from fftModule import fft, ifft
+import fftModule as fM
 from scipy.signal import find_peaks
 
 A = 1
 fs = 1024
 f = 100
-N = 1024
+N = 1000
 
 t = np.arange(N) / fs
 
 x = A * np.sin(2 * np.pi * f * t)
 freq = np.arange(N) * fs / N
 
-fftredix2 = fft.bluestein(x)
-ifftredix2 = ifft.ifft(fftredix2)
+fftredix2 = fM.fft.bluestein(x)
+ifftredix2 = fM.ifft.ifft(fftredix2)
 
 magnitude = 2 * np.abs(fftredix2) / N
 peaks, _ = find_peaks(magnitude, height=0.1)
